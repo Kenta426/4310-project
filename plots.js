40818636ee55ac60a9e9391302d7ef9d7450aaa7
Kenta Takatsu <kt426@cornell.edu>
2018-03-09 17:28:32 -0500

    dynamic query and transition

--- plots.js
+++ plots.js
@@ -1,6 +1,6 @@
 function radial_scatter(data, svg){
   var temp = data;
-  var PADDING = 100;
+  var PADDING = 50;
   var width = 900;
   var height = 900;
   var centroid = svg.append('g')
@@ -21,17 +21,16 @@ function radial_scatter(data, svg){
 
   var r = d3.scaleLog()
   .domain(d3.extent(data, d=>d.watched))
-  .range([0.1,12]);
+  .range([1,7]);
 
 
-  var color = temp.filter(function(d){
-    return ! isNaN(d.dominant_hsl.h)
-  });
+  // var color = temp.filter(function(d){
+  //   return ! isNaN(d.dominant_hsl.h)
+  // });
   // console.log(color);
 
-
   centroid.selectAll('.movie_plot')
-  .data(color)
+  .data(temp)
   .enter()
     .append('circle')
     .attr('class', 'movie_plot')
@@ -39,97 +38,117 @@ function radial_scatter(data, svg){
     .attr('cy', d => Math.sin(y(d.hue_loc)*Math.PI/180)*x(d.dominant_hsl.l))
     .attr('r', d => r(d.watched))
     .on('mouseover', function(d){
-      console.log(d);
+      if (d.show){
+        console.log(d.title);
+      }
     })
     .attr('fill', function(d){
       if (d.show){
-        return d3.hsl(d.dominant_sat)
+        return d3.hsl(d.dominant_hsl)
       }
       return 'None'
     })
-    .attr('opacity', 0.6)
+    .style('fill-opacity', 0.6)
     .style('stroke', 'white')
-    .style('stroke-width', '1')
+    .style('stroke-width', '0.2')
     .style('stroke-opacity', function(d){
-      if (d.show){
-        return 0.5;
-      }
-      return 0.1
+      return 0.5
     })
 }
 
-function filter_genre(data, svg, genre){
-  svg.selectAll(".movie_plot").remove();
-  var temp = data.map(
-    function(d){
-      d.show = d.genre.includes(genre);
-      return d
-    });
-  // console.log(temp);
-  scatter(temp, svg);
-}
-
-
-function filter_year(data, svg, year){
-  svg.selectAll(".movie_plot").remove();
-  var temp = data.map(
-    function(d){
-      d.show = (d.year === year);
-      return d
-    });
-  // console.log(temp);
-  scatter(temp, svg);
-}
-
-function filter_decade(data, svg, decade){
-  years = [];
-  for (var i = 0; i < 10; i++){
-    years.push(decade + i);
-  };
-  svg.selectAll(".movie_plot").remove();
-  var temp = data.map(
-    function(d){
-      d.show = (years.includes(d.year));
-      return d
-    });
-  // console.log(temp);
-  scatter(temp, svg);
-}
-
 function filter_genre_r(data, svg, genre){
-  svg.selectAll(".movie_plot").remove();
-  var temp = data.map(
-    function(d){
-      d.show = d.genre.includes(genre);
-      return d
-    });
-  // console.log(temp);
-  radial_scatter(temp, svg);
-}
+  var PADDING = 100;
+  var width = 900;
+  var height = 900;
+  var r = d3.scaleLog()
+  .domain(d3.extent(data, d=>d.watched))
+  .range([1,14]);
+
+  var filtered = data.map(function(d){
+    d.show = d.genre.includes(genre);
+    return d
+  });
+  // JOIN new data with old elements.
+  var newplot = svg.selectAll(".movie_plot")
+    .data(filtered);
+
+  newplot.exit().remove();
+
+  newplot
+  .transition()
+  .ease(d3.easeCubic).duration(1000)
+  .style('stroke-width', function(d){
+    if (d.show){
+      return 1
+    }
+    else{
+      return 0.1
+    }
+  })
+  .style('fill-opacity', function(d){
+    if (d.show){
+      return 0.8
+    }
+    else{
+      return 0
+    }
+  })
+  .attr('r', function(d){
+    if (d.show){
+      // this.parentNode.appendChild(this);
+      return r(d.watched)
+    }
+    else{
+      return 2;
+    }
+  });
+};
 
 
 function filter_year_r(data, svg, year){
-  svg.selectAll(".movie_plot").remove();
-  var temp = data.map(
-    function(d){
-      d.show = (d.year === year);
-      return d
-    });
-  // console.log(temp);
-  radial_scatter(temp, svg);
-}
+  var PADDING = 100;
+  var width = 900;
+  var height = 900;
+  var r = d3.scaleLog()
+  .domain(d3.extent(data, d=>d.watched))
+  .range([1,21]);
 
-function filter_decade_r(data, svg, decade){
-  years = [];
-  for (var i = 0; i < 10; i++){
-    years.push(decade + i);
-  };
-  svg.selectAll(".movie_plot").remove();
-  var temp = data.map(
-    function(d){
-      d.show = (years.includes(d.year));
-      return d
-    });
-  // console.log(temp);
-  radial_scatter(temp, svg);
-}
+  var filtered = data.map(function(d){
+    d.show = (d.year==year);
+    return d
+  });
+  // JOIN new data with old elements.
+  var newplot = svg.selectAll(".movie_plot")
+    .data(filtered);
+
+  newplot.exit().remove();
+
+  newplot
+  .transition()
+  .ease(d3.easeCubic).duration(1000)
+  .style('stroke-width', function(d){
+    if (d.show){
+      return 1
+    }
+    else{
+      return 0.1
+    }
+  })
+  .style('fill-opacity', function(d){
+    if (d.show){
+      return 0.8
+    }
+    else{
+      return 0
+    }
+  })
+  .attr('r', function(d){
+    if (d.show){
+      // this.parentNode.appendChild(this);
+      return r(d.watched)
+    }
+    else{
+      return 2;
+    }
+  });
+};
